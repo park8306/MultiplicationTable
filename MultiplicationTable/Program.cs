8c32e15182abdd786c5fe973e004a9d769d6fcbd
park8306 <kosaf0804@gmail.com>
2021-04-21 19:29:10 +0900

    초기 커밋.

---
+++ MultiplicationTable/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MultiplicationTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("출력할 구구단을 입력하세요");
+
+            string readString = Console.ReadLine();
+
+            int number = int.Parse(readString);
+
+            for (int i = 0; i < 9; i++)
+            {
+                Console.WriteLine($"{number} X {i + 1} = {number * (i + 1)}");
+            }
+        }
+    }
+}
