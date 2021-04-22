17b877664e7bacffa13fc35e68bab5a0fd306e15
park8306 <kosaf0804@gmail.com>
2021-04-22 17:02:29 +0900

    Revert "십오단 출력"
    This reverts commit 3cbe48359ce99d53db6014abb9ebc8030e25a091.

--- MultiplicationTable/Program.cs
+++ MultiplicationTable/Program.cs
@@ -6,13 +6,13 @@ namespace MultiplicationTable
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("출력할 십오단을 입력하세요");
+            Console.WriteLine("출력할 구구단을 입력하세요");
 
             string readString = Console.ReadLine();
 
             int number = int.Parse(readString);
 
-            for (int i = 0; i < 15; i++)
+            for (int i = 0; i < 9; i++)
             {
                 Console.WriteLine($"{number} X {i + 1} = {number * (i + 1)}");
             }
